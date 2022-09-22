3efc794d363eeea59da05d796012559fefb33e9c
Gabdullin_Roman <romaga59@gmail.com>
2022-09-23 00:25:16 +0500

    Исправил имя папки с Task03 на Task04 в которой решение 4 задачи

---
+++ Task04/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.Write($"Наибольшее = {num1}");
+    }
+    else
+    {
+        Console.Write($"Наибольшее = {num3}");
+    }
+}
+else
+{
+    if (num2 > num3)
+    {
+        Console.Write($"Наибольшее = {num2}");
+    }
+    else
+    {
+        Console.Write($"Наибольшее = {num3}");
+    }
+}
