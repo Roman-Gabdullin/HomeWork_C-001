73be468ebc7a963d374cf1de5a7cbadd8fa2991b
Gabdullin_Roman <romaga59@gmail.com>
2022-09-24 17:27:38 +0500

    Решил 6 задачу

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите целое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1 % 2;
+if (num2 == 0)
+{
+    Console.Write ("Четное");
+}
+else
+{
+    Console.Write ("Нечетное");
+}
