2de5493338491d17b55b429eff7eaf94554d3c50
Gabdullin_Roman <romaga59@gmail.com>
2022-09-24 17:54:39 +0500

    Решил 8 задачу

---
+++ Task08/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите целое число:");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+int num2 = 0;
+while (num2 <= num1 )
+{
+    num2 = num2 + 2;
+    if (num2 <= num1)
+    {
+    Console.WriteLine ($"{num2} ");
+    }
+}
